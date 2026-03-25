25b57ba68542c6b35a67ec7d27186aa0b69f1404
Nguyen Hong Son <snguyenhong8@gmail.com>
2026-03-25 23:31:37 +0700

    feat: Set Up font in next-app

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -2,9 +2,11 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark">
-      <Button>Hello World</Button>
-      <h1>Hihi</h1>
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark google-sans-flex-regular">
+      <Button className="font-bold text-3xl">Hello World</Button>
+      <p className="google-sans-flex-regular font-bold text-5xl">
+        Nội dung dùng Google Sans Flex
+      </p>
     </div>
   );
 }
