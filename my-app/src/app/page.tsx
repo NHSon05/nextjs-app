df67d4b4c9b546994dce0ae6428c777fc59e4b97
Nguyen Hong Son <snguyenhong8@gmail.com>
2026-03-25 23:42:35 +0700

    image in next-js

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,12 +1,14 @@
-import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark google-sans-flex-regular">
-      <Button className="font-bold text-3xl">Hello World</Button>
-      <p className="google-sans-flex-regular font-bold text-5xl">
-        Nội dung dùng Google Sans Flex
-      </p>
+      <Image
+        src='/images/legal.jpg'
+        alt="legal"
+        width={500}
+        height={500}
+      />
     </div>
   );
 }
