974d57a64e57b39787093fd283c0a7a19a3929fb
Nguyen Hong Son <snguyenhong8@gmail.com>
2026-06-20 18:15:09 +0700

    remove dist

--- nextjs-free/server/dist/constants/error-reference.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PrismaErrorCode = void 0;
-exports.PrismaErrorCode = {
-    UniqueConstraintViolation: 'P2002',
-    RecordNotFound: 'P2025'
-};
--- nextjs-free/server/dist/plugins/auth.plugins.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-// import fastifyPlugin from 'fastify-plugin'
-Object.defineProperty(exports, "__esModule", { value: true });
-// export const verifyEmailVerifyTokenPlugin = fastifyPlugin(async (fastify) => {
-//   fastify.decorateRequest('EVTayload', null)
-// })
